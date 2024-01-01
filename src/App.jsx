0d6ce13f2679a6e1244f0dc7ddfdb978bf5aa03b
E-K8 <ekatplat@gmail.com>
2024-01-01 22:01:03 +0000

    add test link, audioClips file, start working on logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css';
+import audioClips from './assets/audioClips';
 
 function App() {
   return (
     <div className='container'>
       <h1>Drum Machine</h1>
+      <section>
+        {audioClips.map((clip) => {
+          <Pad key={(clip, id)} clip={clip} />;
+        })}
+      </section>
+    </div>
+  );
+}
+
+function Pad({ clip }) {
+  return (
+    <div>
+      <audio id={clip.keyTrigger} src={clip.url}></audio>
+      {clip.keyTrigger}
     </div>
   );
 }
